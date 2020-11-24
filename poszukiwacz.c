05a3eaa864f369380ba4681c4878db806f23816f
Filip Sądowicz <sadowicz31@gmail.com>
2020-11-24 18:45:31 +0100

    Adding resource file marking mechanism

--- poszukiwacz.c
+++ poszukiwacz.c
@@ -18,11 +18,20 @@ int parseArgs(int argc, char** argv, char** resourceName, char** fifoPath);
 
 void waitForParent();
 
-int checkEnd(char* fifoPath);
+int checkEnd();
 
-int obtainResource(char* resource);
-char* createResourceFileName(char* resource, int resourceNumber);
-int createFile(char* name, int resourcesObtained);
+int obtainResource();
+char* createResourceFileName(long resourceNumber);
+int createFile(char* name);
+
+void markResource(int status, void* resourceNumber);
+char* createMarkedName();
+
+char* procName = NULL;
+char* resourceName = NULL;
+char* fifoPath = NULL;
+
+int resourcesObtained = 0;
 
 int main(int argc, char* argv[])
 {
@@ -32,8 +41,7 @@ int main(int argc, char* argv[])
 		return EXIT_FAILURE;
 	}
 
-	char* resourceName = NULL;
-	char* fifoPath = NULL;
+	procName = argv[0];
 
 	if(!parseArgs(argc, argv, &resourceName, &fifoPath))
 		return EXIT_FAILURE;
@@ -95,7 +103,7 @@ void waitForParent()
 	close(4);
 }
 
-int checkEnd(char* fifoPath)
+int checkEnd()
 {
 	int res = 1;
 
@@ -116,24 +124,29 @@ int checkEnd(char* fifoPath)
 	return res;
 }
 
-int obtainResource(char* resource)
+int obtainResource()
 {
 	int res = 1;
 
-	int resObtained = 0;
-
-	for(int resNumber = 10; resNumber <= 99; resNumber++)
+	for(long resNumber = 10; resNumber <= 99; resNumber++)
 	{
-		char* name = createResourceFileName(resource, resNumber);
+		char* name = createResourceFileName(resNumber);
 		if(!(res = (name != NULL)))
 			break;
 
 		if(access(name, F_OK))
 		{	
-			if(!(res = createFile(name, ++resObtained)))
-				break;
+			int crFileRes = createFile(name); 
+	
+			if(crFileRes == 1)
+			{
+				if(!(res = on_exit(markResource, (void*)resNumber) == 0))
+					fprintf(stderr, "ERROR: obtainResource: Unable to register function\n");
+			}
+			else if(crFileRes == -1)
+				res = 0;
 
-			//TODO register
+			break;
 		}
 
 		free(name);
@@ -142,13 +155,13 @@ int obtainResource(char* resource)
 	return res;
 }
 
-char* createResourceFileName(char* resource, int resourceNumber)
+char* createResourceFileName(long resourceNumber)
 {
 	char* res = NULL;
-	res = (char*)malloc(strlen(resource) + 5);
+	res = (char*)malloc(strlen(resourceName) + 18);
 	if(res)
 	{
-		if(sprintf(res, "%s_0%d", resource, resourceNumber) < 0)
+		if(sprintf(res, "property_0%ld.%smine", resourceNumber, resourceName) < 0)
 		{
 			fprintf(stderr, "ERROR: createResourceFileName: Unable to write filename into buffer\n");
 			res = NULL;
@@ -160,14 +173,14 @@ char* createResourceFileName(char* resource, int resourceNumber)
 	return res;
 }
 
-int createFile(char* name, int resourcesObtained)
+int createFile(char* name)
 {	
 	int res = 1;
 	errno = 0;
 	int fd = open(name, O_WRONLY | O_CREAT | O_EXCL, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH );
-	if(fd != -1)
+	if((res = (fd != -1)))
 	{
-		if(!(res = (truncate(name, resourcesObtained * UNIT) != -1)))
+		if(!(res = (truncate(name, (++resourcesObtained) * UNIT) != -1)))
 			fprintf(stderr, "ERROR: createFile: Unable to set file size\n");
 
 		close(fd);
@@ -175,8 +188,36 @@ int createFile(char* name, int resourcesObtained)
 	else if(errno != EEXIST)
 	{
 		fprintf(stderr, "ERROR: createFile: Unable to open file descriptor\n");
-		res = 0;
+		res = -1;
 	}
+
+	return res;
+}
+
+void markResource(int status, void* resourceNumber)
+{
+	long number = (long)resourceNumber;
+
+	char* oldName = createResourceFileName(number);
+	char* newName = createMarkedName();
+
+	rename(oldName, newName);
+}
+
+char* createMarkedName()
+{
+	char* res = NULL;
+	res = (char*)malloc(strlen(procName) + strlen(resourceName) + 10);
+	if(res)
+	{
+		if(sprintf(res, "%s_%smine.#%d", procName, resourceName, resourcesObtained--) < 0)
+		{
+			fprintf(stderr, "ERROR: createMarkedName: Unable to write filename into buffer\n");
+			res = NULL;
+		}
+	}
+	else
+		fprintf(stderr, "ERROR: createMarkedName: Unable to allocate buffer for file name\n");
 
 	return res;
 }
