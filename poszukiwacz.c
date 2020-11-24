f207c61285c26a94800c57bb0599d6100ff545c7
Filip Sądowicz <sadowicz31@gmail.com>
2020-11-24 16:21:52 +0100

    adding resource obtaining mechanism

--- poszukiwacz.c
+++ poszukiwacz.c
@@ -1,8 +1,12 @@
-#define _POSIX_C_SOURCE 2
-#define _XOPEN_SOURCE
+#define _POSIX_C_SOURCE 200809L
+#define _XOPEN_SOURCE 500
+#define _DEFAULT_SOURCE
+
+#define UNIT 384
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include <unistd.h>
 #include <sys/types.h>
@@ -15,6 +19,10 @@ int parseArgs(int argc, char** argv, char** resourceName, char** fifoPath);
 void waitForParent();
 
 int checkEnd(char* fifoPath);
+
+int obtainResource(char* resource);
+char* createResourceFileName(char* resource, int resourceNumber);
+int createFile(char* name, int resourcesObtained);
 
 int main(int argc, char* argv[])
 {
@@ -36,13 +44,13 @@ int main(int argc, char* argv[])
 
 	printf("started: %s ( resource: %s\tfifo: %s )\n", argv[0], resourceName, fifoPath);
 
-	int counter;
 	while(checkEnd(fifoPath))
 	{
-		counter++;
+		if(!obtainResource(resourceName))
+			return EXIT_FAILURE;
 	}
 
-	printf("ended: %s ( counter: %d )\n", argv[0], counter);
+	printf("ended: %s\n", argv[0]);
 	
 	return EXIT_SUCCESS;
 }
@@ -102,6 +110,71 @@ int checkEnd(char* fifoPath)
 	else if(errno != ENXIO)
 	{
 		fprintf(stderr, "ERROR: checkEnd: Unrecognized error while opening fifo file\n");
+		res = 0;
+	}
+
+	return res;
+}
+
+int obtainResource(char* resource)
+{
+	int res = 1;
+
+	int resObtained = 0;
+
+	for(int resNumber = 10; resNumber <= 99; resNumber++)
+	{
+		char* name = createResourceFileName(resource, resNumber);
+		if(!(res = (name != NULL)))
+			break;
+
+		if(access(name, F_OK))
+		{	
+			if(!(res = createFile(name, ++resObtained)))
+				break;
+
+			//TODO register
+		}
+
+		free(name);
+	}
+
+	return res;
+}
+
+char* createResourceFileName(char* resource, int resourceNumber)
+{
+	char* res = NULL;
+	res = (char*)malloc(strlen(resource) + 5);
+	if(res)
+	{
+		if(sprintf(res, "%s_0%d", resource, resourceNumber) < 0)
+		{
+			fprintf(stderr, "ERROR: createResourceFileName: Unable to write filename into buffer\n");
+			res = NULL;
+		}
+	}
+	else
+		fprintf(stderr, "ERROR: createResourceFileName: Unable to allocate buffer for file name\n");
+
+	return res;
+}
+
+int createFile(char* name, int resourcesObtained)
+{	
+	int res = 1;
+	errno = 0;
+	int fd = open(name, O_WRONLY | O_CREAT | O_EXCL, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH );
+	if(fd != -1)
+	{
+		if(!(res = (truncate(name, resourcesObtained * UNIT) != -1)))
+			fprintf(stderr, "ERROR: createFile: Unable to set file size\n");
+
+		close(fd);
+	}
+	else if(errno != EEXIST)
+	{
+		fprintf(stderr, "ERROR: createFile: Unable to open file descriptor\n");
 		res = 0;
 	}
 
